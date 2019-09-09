ff67d2efce6119bc1b16fa210ba6c789b830dcaf
hisuica <hisucal@gmail.com>
2019-09-10 03:01:45 +0900

    first Test commit

--- server/src/main/java/com/hisuica/cafeapp/cafeApp/controller/MainController.java
+++ server/src/main/java/com/hisuica/cafeapp/cafeApp/controller/MainController.java
@@ -1,21 +1,33 @@
 package com.hisuica.cafeapp.cafeApp.controller;
 
 import com.hisuica.cafeapp.cafeApp.model.Notice;
-import com.hisuica.cafeapp.cafeApp.repository.MainRepository;
+import com.hisuica.cafeapp.cafeApp.repository.NoticeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
-    private MainRepository repository;
+    private NoticeRepository repository;
 
-    public MainController(MainRepository repository) { this.repository = repository; }
+    public MainController(NoticeRepository repository) { this.repository = repository; }
 
     @GetMapping("/")
     public Collection<Notice> notices() {
+
+        Notice notice = Notice.builder()
+                .title("testTitle")
+                .context("testContext")
+                .type("Event")
+                .insertDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        repository.save(notice);
+
         return repository.findAll().stream()
                         .filter(this::isValid)
                         .collect(Collectors.toList());
